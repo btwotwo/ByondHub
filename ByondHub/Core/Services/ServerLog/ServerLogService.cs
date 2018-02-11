cdd0c7e00023efbe3bbab4d6aa3b2b874ce8f0f8
bo20202 <bo20202@yandex.ru>
2018-02-11 19:37:51 +0300

    Server logs stub

---
+++ ByondHub/Core/Services/ServerLog/ServerLogService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ByondHub.Core.Services.ServerLog
+{
+    public class ServerLogService : IServerLogService
+    {
+        public FileStream GetSaylog(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FileStream GetRuntimeLog(DateTime? date = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
