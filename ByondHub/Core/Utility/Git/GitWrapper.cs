bc2aafaff0b57c96dc743881d0c7b512a6374b8a
bo20202 <bo20202@yandex.ru>
2018-09-02 18:21:33 +0300

    Add GitWrapper stub

---
+++ ByondHub/Core/Utility/Git/GitWrapper.cs
@@ -0,0 +1,14 @@
+﻿
+using LibGit2Sharp;
+using PullOptions = ByondHub.Core.Utility.Git.Models.PullOptions;
+
+namespace ByondHub.Core.Utility.Git
+{
+    public interface IGitWrapper
+    {
+        void Pull(Repository repo, PullOptions options);
+    }
+    public class GitWrapper
+    {
+    }
+}
---
+++ ByondHub/Core/Utility/Git/Models/PullOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace ByondHub.Core.Utility.Git.Models
+{
+    public class PullOptions
+    {
+        public string RepositoryPath { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
