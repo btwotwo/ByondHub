8fd71de69be52050eb1673dd8fc52dd05eb2d4ff
bo20202 <bo20202@yandex.ru>
2018-09-22 18:52:24 +0300

    Set default invisible param to false

--- ByondHub/Core/Utility/Byond/DreamDaemonArguments.cs
+++ ByondHub/Core/Utility/Byond/DreamDaemonArguments.cs
@@ -10,7 +10,7 @@ namespace ByondHub.Core.Utility.Byond
         public string ExecutablePath { get; set; }
         public ushort Port { get; set; }
         public bool Safe { get; set; } = true;
-        public bool Invisible { get; set; } = true;
+        public bool Invisible { get; set; } = false;
         public bool LogSelf { get; set; } = true;
     }
 }
